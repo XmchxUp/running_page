b3f206ea15c2b724f40b965caad96cf8b49c8581
DuckTyping <1394466835@qq.com>
2023-04-06 13:37:11 +0800

    feat: update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,17 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    siteUrl: 'https://running-page-xmchxup.vercel.app/',
+    logo: 'https://cdn.jsdelivr.net/gh/XmchxUp/cloudimg@master/partywizard.4ar7hnvogr00.gif',
+    description: "You'll see that better future.",
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://xmchxup.github.io/',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://github.com/XmchxUp',
       },
     ],
   },
