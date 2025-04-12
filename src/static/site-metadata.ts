7216f9195a6acd43521a1cfab0ee19528d3e4ec7
ultraman <1394466835@qq.com>
2025-04-12 09:20:18 +0800

    update

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,9 +16,9 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  siteUrl: 'https://xmchxup.github.io/running_page/',
+  logo: 'https://avatars.githubusercontent.com/u/39235427?v=4',
+  description: 'Running Page',
   navLinks: [
     {
       name: 'Summary',
@@ -26,11 +26,11 @@ const data: ISiteMetadataResult = {
     },
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://xmchx.vercel.app',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://xmchx.vercel.app/post/about',
     },
   ],
 };
